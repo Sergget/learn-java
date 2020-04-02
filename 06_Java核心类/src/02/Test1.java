f6b8a4560d205bbdbda138a6c26382349b83646b
sergget <sergget.yang@outlook.com>
2020-04-02 22:05:27 +0800

    String类

---
+++ 06_Java核心类/src/02/Test1.java
@@ -0,0 +1,9 @@
+
+public class Test1 {
+    public static void main(String[] args) {
+        String s = "Hello";
+        System.out.println(s);
+        s = s.toUpperCase();
+        System.out.println(s);
+    }
+}
