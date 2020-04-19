6c69bd7c0c18eff49a31120f95e50365ab9aff4f
sergget <sergget.yang@outlook.com>
2020-04-20 07:57:27 +0800

    练习

--- 08_反射/src/03/Main.java
+++ 08_反射/src/03/Main.java
@@ -1,9 +1,17 @@
+import java.lang.reflect.Method;
+
 public class Main {
 	public static void main(String[] args) throws Exception {
 		String name = "Xiao Ming";
 		int age = 20;
 		Person p = new Person();
 		// TODO: 利用反射调用setName和setAge方法:
+
+		Method mSetName = Person.class.getMethod("setName",String.class);
+		Method mSetAge = Person.class.getMethod("setAge",int.class);
+
+		mSetName.invoke(p, name);
+		mSetAge.invoke(p, age);
 
 		System.out.println(p.getName()); // "Xiao Ming"
 		System.out.println(p.getAge()); // 20
