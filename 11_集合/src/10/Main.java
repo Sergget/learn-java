8f4aaa622aa0d16f956e0155b8bb3185aee7117c
sergget <sergget.yang@outlook.com>
2020-05-12 08:00:34 +0800

    练习

--- 11_集合/src/10/Main.java
+++ 11_集合/src/10/Main.java
@@ -5,8 +5,8 @@ public class Main {
     public static void main(String[] args) {
         Queue<User> q = new PriorityQueue<>(new UserComparator());
         // 添加3个元素到队列:
-        q.offer(new User("Bob", "A1"));
-        q.offer(new User("Alice", "A2"));
+        q.offer(new User("Bob", "A2"));
+        q.offer(new User("Alice", "A10"));
         q.offer(new User("Boss", "V1"));
         System.out.println(q.poll()); // Boss/V1
         System.out.println(q.poll()); // Bob/A1
@@ -19,7 +19,9 @@ class UserComparator implements Comparator<User> {
     public int compare(User u1, User u2) {
         if (u1.number.charAt(0) == u2.number.charAt(0)) {
             // 如果两人的号都是A开头或者都是V开头,比较号的大小:
-            return u2.number.compareTo(u1.number);
+            Integer n1 = Integer.parseInt(u1.number.substring(1));
+            Integer n2 = Integer.parseInt(u2.number.substring(1));
+            return n1.compareTo(n2);
         }
         if (u1.number.charAt(0) == 'V') {
             // u1的号码是V开头,优先级高:
