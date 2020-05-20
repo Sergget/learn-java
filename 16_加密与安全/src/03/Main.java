5d42ef609c2f0023f474633722397d22df9aa9fa
sergget <sergget.yang@outlook.com>
2020-05-20 16:32:33 +0800

    boucncyCastle

---
+++ 16_加密与安全/src/03/Main.java
@@ -0,0 +1,17 @@
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.Security;
+
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		// 注册BouncyCastle:
+		Security.addProvider(new BouncyCastleProvider());
+		// 按名称正常调用:
+		MessageDigest md = MessageDigest.getInstance("RipeMD160");
+		md.update("HelloWorld".getBytes("UTF-8"));
+		byte[] result = md.digest();
+		System.out.println(new BigInteger(1, result).toString(16));
+	}
+}
